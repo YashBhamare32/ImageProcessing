07cf048696bbbe1327f381d546532b36caa5376c
YashBhamare32 <bobbybhamare32@gmail.com>
2024-06-21 01:17:00 +0530

    microservices initialized

---
+++ authentication-service/src/auth/auth.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { JwtModule } from '@nestjs/jwt';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { UserSchema } from './schemas/user.schema';
+import { jwtConstants } from './constants';
+
+@Module({
+    imports:[
+        MongooseModule.forFeature([{name : "Users" , schema : UserSchema}]),
+        JwtModule.register({
+            secret:jwtConstants.secret,
+            signOptions:{
+              expiresIn:"60m"
+            }
+        })
+    ],
+    providers:[AuthService],
+    controllers:[AuthController]
+})
+export class AuthModule {}

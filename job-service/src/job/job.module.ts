333500c1720a226d6f429229cbb4816160add876
YashBhamare32 <bobbybhamare32@gmail.com>
2024-06-23 23:54:55 +0530

    minor

--- job-service/src/job/job.module.ts
+++ job-service/src/job/job.module.ts
@@ -2,21 +2,24 @@ import { Module } from '@nestjs/common';
 import { JobService } from './job.service';
 import { JobController } from './job.controller';
 import { JwtModule } from '@nestjs/jwt';
-import { BlobService } from 'src/blob/blob.service';
-import { BlobModule } from 'src/blob/blob.module';
+// import { BlobService } from 'src/blob/blob.service';
+// import { BlobModule } from 'src/blob/blob.module';
 import { MongooseModule } from '@nestjs/mongoose';
-import { blobSchema } from 'src/auth/schemas/blob.schema';
+// import { blobSchema } from 'src/auth/schemas/blob.schema';
 import { jwtConstants } from './constants';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
-  imports:[JwtModule.register({
+  imports:[
+    ClientsModule.register([{name : "BLOB_SERVICE" , transport: Transport.TCP , options: {host:"blob-service" , port:3001} }]),
+    JwtModule.register({
     secret:jwtConstants.secret,
     signOptions:{
       expiresIn:"60m"
     }
   }),
-  BlobModule,
-  MongooseModule.forFeature([{name:"Blob" , schema:blobSchema}])
+  // BlobModule,
+  // MongooseModule.forFeature([{name:"Blob" , schema:blobSchema}])
 ],
   providers: [JobService],
   controllers: [JobController]
--- job-service/src/job/job.service.ts
+++ job-service/src/job/job.service.ts
@@ -1,17 +1,16 @@
-import { Headers, Injectable, NotFoundException, Request } from '@nestjs/common';
-import { JobDto } from './dto/job.dto';
-import { Users } from 'src/auth/schemas/user.schema';
-import { BlobService } from 'src/blob/blob.service';
+import { Headers, Inject, Injectable, NotFoundException, Request } from '@nestjs/common';
+// import { JobDto } from './dto/job.dto';
+// import { Users } from 'src/auth/schemas/user.schema';
+// import { BlobService } from 'src/blob/blob.service';
 import * as fs from "fs";
 import { InjectModel } from '@nestjs/mongoose';
-import { Blob, blobSchema } from 'src/auth/schemas/blob.schema';
+// import { Blob, blobSchema } from 'src/auth/schemas/blob.schema';
 import { Model } from 'mongoose';
+import { ClientProxy } from '@nestjs/microservices';
 
 @Injectable()
 export class JobService {
-    constructor(private blobService : BlobService,
-        @InjectModel(Blob.name) private blobModel: Model<Blob>
-    ){}
+    constructor(@Inject("BLOB_SERVICE") private readonly blobServiceClient : ClientProxy){}
     async createJob(image:Express.Multer.File , user:Users ,  headers:any){
 
         //convert to base64 encoding
@@ -21,22 +20,24 @@ export class JobService {
         const token = headers.authorization.split(' ')[1];
         console.log(token);
 
-        const res = await this.blobService.storeImage(token , base64Image);
+        // const res = await this.blobService.storeImage(token , base64Image);
 
-        await fs.promises.unlink(image.path);
-        return res;
+        // await fs.promises.unlink(image.path);
+        return this.blobServiceClient.send({ cmd : "storeImage" } , {token , base64Image}).toPromise();
     } 
 
     async getJob(params:any){
-        const id = params.id;
-        const job:Blob = await this.blobModel.findOne({id});
-        if(!job){
-            throw new NotFoundException("Job not found in db");
-        }
-        console.log(job);
-        return {
-            id: job.id,
-            status:job.status
-        };
+        // const id = params.id;
+        // const job:Blob = await this.blobModel.findOne({id});
+        // if(!job){
+        //     throw new NotFoundException("Job not found in db");
+        // }
+        // console.log(job);
+        // return {
+        //     id: job.id,
+        //     status:job.status
+        // };
+
+        return this.blobServiceClient.send({cmd: 'getBlob'} , {params}).toPromise();
     }
 }

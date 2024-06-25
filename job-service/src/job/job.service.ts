874953a6f7314f43bd924ecf878625bd801e43d6
YashBhamare32 <bobbybhamare32@gmail.com>
2024-06-25 12:00:38 +0530

    job and blob services setup complete

--- job-service/src/job/job.service.ts
+++ job-service/src/job/job.service.ts
@@ -1,14 +1,12 @@
 import { Headers, Inject, Injectable, NotFoundException, Request } from '@nestjs/common';
-// import { JobDto } from './dto/job.dto';
-// import { Users } from 'src/auth/schemas/user.schema';
-// import { BlobService } from 'src/blob/blob.service';
 import * as fs from "fs";
 import { InjectModel } from '@nestjs/mongoose';
-// import { Blob, blobSchema } from 'src/auth/schemas/blob.schema';
 import { Model } from 'mongoose';
 import { ClientProxy } from '@nestjs/microservices';
 import {HttpService } from "@nestjs/axios";
 import { lastValueFrom } from 'rxjs';
+import { AxiosRequestConfig } from 'axios';
+import { Response } from 'express';
 
 @Injectable()
 export class JobService {
@@ -20,32 +18,44 @@ export class JobService {
 
         //convert to base64 encoding
         const buffer = await fs.promises.readFile(image.path);
-        const base64Image = buffer.toString("base64");
+        const base64 = buffer.toString("base64");
 
         const token = headers.authorization.split(' ')[1];
         console.log(token);
-        const res = await lastValueFrom(
-            this.httpService.post('http://localhost:3002/blob', { base64Image, token })
-          );
-        console.log(res.data);
-        return res.data;
-        // const res = await this.blobService.storeImage(token , base64Image);
 
-        // await fs.promises.unlink(image.path);
+        const config: AxiosRequestConfig = {
+            url : "http://localhost:3002/blob",
+            method : "post",
+            data : {base64 , token},
+          };
+        try {
+            const res = await this.httpService.axiosRef.request(config);
+            console.log(res.data);
+            return res.data;
+        } catch (error) {
+            throw new Error(error);
+        }
     } 
 
-    async getJob(id:any){
-        // const id = params.id;
-        // const job:Blob = await this.blobModel.findOne({id});
-        // if(!job){
-        //     throw new NotFoundException("Job not found in db");
-        // }
-        // console.log(job);
-        // return {
-        //     id: job.id,
-        //     status:job.status
-        // };
-
-        return this.blobServiceClient.send({cmd: 'getBlob'} , id).toPromise();
+    async getJobStatus(id:any , res:Response){
+        console.log(id);
+        const config: AxiosRequestConfig = {
+            url : "http://localhost:3002/blob/"+id,
+            method : "get",
+          };
+        try {
+            const resp = await this.httpService.axiosRef.request(config);
+            console.log(resp.data);
+            return res.json({
+                "JobId" : id,
+                "status":resp.data.status
+            });
+        } catch (error) {
+            console.log(res);
+            return res.json({
+                "Msg":"Job not found",
+                "error":error
+            })
+        }
     }
 }
